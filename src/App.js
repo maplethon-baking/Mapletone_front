610bdb28149dbe8a4e6094acd6992d184ff2755c
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2022-11-15 13:42:13 +0900

    setting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Boards from "./routers/Boards";
+import Home from "./routers/home";
+import Recipe from "./routers/Recipe";
+import Search from "./routers/Search";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/boards" element={<Boards />} />
+        <Route path="/recipe" element={<Recipe />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
