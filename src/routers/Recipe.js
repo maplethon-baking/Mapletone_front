b27defe82cb77e15a589e3ef9b977a8f0dc47f19
c0c0pang <coc1789@naver.com>
2022-11-18 04:11:00 +0900

    feat: 버튼을 클릭시 레시피 페이지로 이동하는 기능 구현

--- src/routers/Recipe.js
+++ src/routers/Recipe.js
@@ -1,22 +1,21 @@
 import { useQuery } from "@tanstack/react-query";
 import { getRecipes } from "../api";
 import { RecipeCard } from '../components/RecipeCard'
-import { Contents } from "./Home";
+import { Container, Contents } from "./Home";
 import create_button from '../assets/create_button.png';
 import speech_bubble from '../assets/speech_bubble.png';
 import styled from "styled-components";
-
+import { useState } from "react";
+import { RecipeCreate } from "../components/RecipeCreate";
 const CreateButton = styled.img`
   width: 50px;
   margin-bottom: 100px;
 `
 const SpeechBubble = styled.img`
-  /* position: relative; */
   width: 220px;
 `
 const SpeechDiv = styled.div`
   margin-top: 30px;
-  vertical-align: middle;
   margin-bottom: 10px;
 `
 const SpeechText = styled.span`
@@ -24,32 +23,33 @@ const SpeechText = styled.span`
   text-align: center;
   transform: translate(6%, 50%);
   color: white; 
-`
-const Container = styled.div`
-  padding: 0px 20px;
-  padding-top: 49px;
-  max-width: 480px;
-  margin: 0 auto;
-  overflow-y: scroll;
-  &::-webkit-scrollbar {
-    display: none;
-  }
-
-  background-color: #f1f1f1;
 `;
+
+
 export function Recipe() {
   const { isLoading, data } = useQuery(["recipe"], getRecipes);
+
+  const onCreateCheck = () => {
+    setCreateCheck(true);
+  };
+  const [createCheck, setCreateCheck] = useState(false);
   return (
-    <Container>
-      <Contents>
-        {data &&
-          data.map((recipes) => <RecipeCard recipes={recipes} key={recipes.id} />)}
-        <SpeechDiv>
-          <SpeechText>나만의 레시피를 만들어보세요!</SpeechText>
-          <SpeechBubble src={speech_bubble} />
-        </SpeechDiv>
-        <CreateButton src={create_button} />
-      </Contents>
-    </Container>
+    <>
+        {
+          createCheck ? (<RecipeCreate />): (
+            <Container>
+            <Contents>
+              {data && data.map((recipes) => <RecipeCard recipes={recipes} key={recipes.id} />)}
+            <SpeechDiv>
+              <SpeechText>나만의 레시피를 만들어보세요!</SpeechText>
+              <SpeechBubble src={speech_bubble} />
+            </SpeechDiv>
+            <CreateButton src={create_button} onClick={onCreateCheck} />
+            </Contents>
+            </Container>
+          )
+      
+      }
+    </>
   );
 }
