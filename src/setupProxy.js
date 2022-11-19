669494e0018954f42bd0d3eb9e001d9c7514c94f
kkzoone <kyungjun1129@naver.com>
2022-11-19 19:19:57 +0900

    테스트 커밋

---
+++ src/setupProxy.js
@@ -0,0 +1,12 @@
+const {createProxyMiddleware} = require('http-proxy-middleware')
+
+module.exports = (app) => {
+    app.use("/api",
+        createProxyMiddleware(
+            {
+                target: "http://localhost:8000",
+                changeOrigin: true,
+            }
+        )
+    )
+};
